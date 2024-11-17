999268db904d45d98ef5edb64ec0ad981ce3eb6f
Saikiran Talla <128293495+saikiran5456@users.noreply.github.com>
2024-11-17 14:18:38 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,12 @@
-"""PlantDisease URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from . import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('PlantDiseaseApp.urls')),
+urlpatterns = [path("index.html", views.index, name="index"),
+	       path('Login.html', views.Login, name="Login"), 
+	       path('Register.html', views.Register, name="Register"),
+	       path('Signup', views.Signup, name="Signup"),
+	       path('UserLogin', views.UserLogin, name="UserLogin"),
+	       path("Upload.html", views.Upload, name="Upload"),
+	       path('UploadImage', views.UploadImage, name="UploadImage"),
 ]
